cea9fbc463259a0f4abb942848984ded834d37aa
Aaron <40146941+Aaronplayz14@users.noreply.github.com>
2024-07-04 13:24:15 -0400

    Delete f23-proj2-car-dot-png directory

--- f23-proj2-car-dot-png/start_mongo.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-mongod --port $1 --dbpath ~/mongodb_data_folder --quiet --logpath /dev/null &
--- f23-proj2-car-dot-png/start_n_load.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-bash start_mongo.sh $1
-python3 load-json.py farmers-protest-tweets-2021-03-5.json $1
